7f926c602ff7b38484c9bcba8f4470386b252670
Arman Lodhra <armanlodhra@Armans-MacBook-Air.local>
2024-11-12 22:30:11 -0800

    updated test files

--- ecommerce-app/product-service/__tests__/productService.test.js
+++ ecommerce-app/product-service/__tests__/productService.test.js
@@ -1,11 +1,14 @@
+
 const request = require('supertest');
-const app = require('../server'); // Import the Express app
+const app = require('../server');
 const sqlite3 = require('sqlite3');
 
 let db;
+let server;
 
 beforeAll(() => {
   db = new sqlite3.Database('./product-database.sqlite');
+  server = app.listen(3003);
 });
 
 beforeEach(async () => {
@@ -17,7 +20,6 @@ beforeEach(async () => {
   });
 });
 
-
 afterAll(async () => {
   await new Promise((resolve, reject) => {
     db.close((err) => {
@@ -25,6 +27,7 @@ afterAll(async () => {
       resolve();
     });
   });
+  server.close();
 });
 
 test('Should add a new product successfully', async () => {
@@ -37,32 +40,27 @@ test('Should add a new product successfully', async () => {
 });
 
 test('Should list all products successfully', async () => {
-  // Add two products first
   await request(app).post('/products').send({ name: 'Product A', price: 99.99 });
   await request(app).post('/products').send({ name: 'Product B', price: 49.99 });
 
-  // Get the list of products
   const response = await request(app).get('/products');
 
   expect(response.statusCode).toBe(200);
   expect(response.body).toBeInstanceOf(Array);
   expect(response.body.length).toBeGreaterThanOrEqual(2);
 
-  // Check if the added products are in the response
   const productNames = response.body.map((product) => product.name);
   expect(productNames).toContain('Product A');
   expect(productNames).toContain('Product B');
 });
 
 test('Should delete a product successfully', async () => {
-  // Add a product first
   const addResponse = await request(app)
     .post('/products')
     .send({ name: 'Product C', price: 29.99 });
 
   const productId = addResponse.body.id;
 
-  // Delete the product
   const deleteResponse = await request(app)
     .delete('/products')
     .send({ id: productId });
@@ -70,7 +68,6 @@ test('Should delete a product successfully', async () => {
   expect(deleteResponse.statusCode).toBe(201);
   expect(deleteResponse.body.message).toBe('Product deleted successfully');
 
-  // Verify the product is deleted
   const getResponse = await request(app).get('/products');
   const products = getResponse.body;
   const productExists = products.some((product) => product.id === productId);
@@ -79,11 +76,7 @@ test('Should delete a product successfully', async () => {
 });
 
 test('Should return 404 for invalid product deletion', async () => {
-  // Attempt to delete a non-existent product
-  const response = await request(app)
-    .delete('/products')
-    .send({ id: 9999 });
-
+  const response = await request(app).delete('/products').send({ id: 9999 });
   expect(response.statusCode).toBe(404);
   expect(response.body).toHaveProperty('error');
 });
